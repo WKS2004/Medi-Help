69155f2afab644c831a1841d3fd243f395bea0eb
Wanshaja Sooriyabandara <159720575+WKS2004@users.noreply.github.com>
2025-05-19 13:07:13 +0530

    Added and Updated Backend Files
    Backend
    - Updated Admin.java
    - Added AddAdminServlet.java
    - Added UpdateAdminServlet.java
    - Added DeleteAdminServlet.java

--- Medi-Help/src/main/java/lk/sliit/project/onlinemedicalstore/medihelp/models/Admin.java
+++ Medi-Help/src/main/java/lk/sliit/project/onlinemedicalstore/medihelp/models/Admin.java
@@ -1,4 +1,11 @@
 package lk.sliit.project.onlinemedicalstore.medihelp.models;
 
-public class Admin {
+public class Admin extends User {
+    public Admin(String username, String firstName, String lastName, String displayName, String password, String email, String address, String phoneNumber) {
+        super(username, firstName, lastName, displayName, password, email, address, phoneNumber, "Admin");
+    }
+
+    public Admin(String username, String firstName, String lastName, String displayName, String password, String email, String address, String phoneNumber, String role) {
+        super(username, firstName, lastName, displayName, password, email, address, phoneNumber, role);
+    }
 }
